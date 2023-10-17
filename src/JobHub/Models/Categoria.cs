fe9023544c9f8d1030d133478b63aa31666340c1
Thiago <thiagogazzinelli@gmail.com>
2023-10-16 21:27:27 -0300

    updatetabela

--- src/JobHub/Models/Categoria.cs
+++ src/JobHub/Models/Categoria.cs
@@ -12,11 +12,7 @@ namespace JobHub.Models
         [StringLength(100, ErrorMessage ="O tamanho máximo é 100 caracteres")]
         [Display(Name ="Nome")]
         public string Nome { get; set; }
-        [Required(ErrorMessage ="Informe a descrição da categoria")]
-        [StringLength(500, ErrorMessage ="O tamanho máximo é 500 caracteres")]
-        [Display(Name ="Descrição")]
-        public string Descricao { get; set; }
-
+        
         public List<Vaga> Vagas { get; set; }
     }
 }
