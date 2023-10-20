6e71bf52b6dec457113a83205220bd513919d044
Matheus Oliveira <matheudev@gmail.com>
2023-10-20 20:39:06 -0300

    HomePage com Listagem de Vagas

--- src/JobHub/Controllers/HomeController.cs
+++ src/JobHub/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using JobHub.Models;
+using JobHub.Repositories.Interfaces;
+using JobHub.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,9 +8,24 @@ namespace JobHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IVagaRepository _vagaRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public HomeController(IVagaRepository vagaRepository, ICategoriaRepository categoriaRepository)
+        {
+            _vagaRepository = vagaRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            ViewData["Title"] = "Home Page";
+
+            var homeIndexViewModel = new HomeIndexViewModel();
+            homeIndexViewModel.Vagas = _vagaRepository.Vagas.OrderByDescending(v => v.Id).ToList();
+            homeIndexViewModel.Categorias = _categoriaRepository.Categorias;
+
+            return View(homeIndexViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
