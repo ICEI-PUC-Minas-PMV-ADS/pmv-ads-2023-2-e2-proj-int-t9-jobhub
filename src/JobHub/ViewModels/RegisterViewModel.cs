37293b7a36f1a318bac44b186b75d19fc387e18c
Matheus Oliveira <matheudev@gmail.com>
2023-11-19 23:15:37 -0300

    Login Fix

--- src/JobHub/ViewModels/RegisterViewModel.cs
+++ src/JobHub/ViewModels/RegisterViewModel.cs
@@ -14,5 +14,17 @@ namespace JobHub.ViewModels
         [Required(ErrorMessage = "Obrigatório Informar a senha!")]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Perfil == Perfil.Candidato && string.IsNullOrWhiteSpace(Candidato?.Nome))
+            {
+                yield return new ValidationResult("Obrigatório Informar o nome do candidato!", new[] { "Candidato.Nome" });
+            }
+            else if (Perfil == Perfil.Empresa && string.IsNullOrWhiteSpace(Empresa?.NomeDaEmpresa))
+            {
+                yield return new ValidationResult("Obrigatório Informar o nome da empresa!", new[] { "Empresa.NomeDaEmpresa" });
+            }
+            // Add other conditional validations here
+        }
     }
 }
