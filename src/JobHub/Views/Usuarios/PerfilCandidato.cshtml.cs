9f03f451e505c370c2a5d71d07ddc546f61a187d
ViniHMariano <116518807+ViniHMariano@users.noreply.github.com>
2023-11-19 22:40:32 -0300

    Update

---
+++ src/JobHub/Views/Usuarios/PerfilCandidato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobHub.Views.Usuarios
+{
+    public class PerfilCandidatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/JobHub/Views/Usuarios/PerfilEmpresa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobHub.Views.Usuarios
+{
+    public class PerfilEmpresaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
